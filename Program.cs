dbab9f1b508cfcb6f4647658533ef54326db562f
Vladimir Izmalkov <vladimir@vaticle.com>
2024-04-18 17:18:21 +0100

    Working tutorial code

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using TypeDB.Driver.Api;
 using TypeDB.Driver.Common;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 class WelcomeToTypeDB
 {
@@ -19,45 +20,58 @@ class WelcomeToTypeDB
     const string CLOUD_PASSWORD = "password";
     // end::constants[]
     // tag::db-schema-setup[]
-    void DbSchemaSetup(IDatabase database, string schemaFile = "iam-schema.tql") {
-        string defineQuery = File.ReadAllText(schemaFile);
-        using (ITypeDBSession session = database.Session(SessionType.Schema)) {
-            using (ITypeDBTransaction transaction = session.Transaction(TransactionType.Write)) {
+    static void DbSchemaSetup(ITypeDBSession session, string schemaFile = "iam-schema.tql") {
+        string defineQuery;
+        try
+        {
+            defineQuery = File.ReadAllText(schemaFile);
+            using (ITypeDBTransaction tx = session.Transaction(TransactionType.Write)) {
                 Console.WriteLine("Defining schema...");
-                transaction.Query.Define(defineQuery).Resolve();
-                transaction.Commit();
+                tx.Query.Define(defineQuery).Resolve();
+                tx.Commit();
                 Console.WriteLine("OK");
             }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Error: File not found.");
+            Environment.Exit(1);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"An error occurred: {e.Message}");
+            Environment.Exit(1);
         }
     }
     // end::db-schema-setup[]
     // tag::db-dataset-setup[]
-    void DbDatasetSetup(IDatabase database, string dataFile = "iam-data-single-query.tql") {
+    static void DbDatasetSetup(ITypeDBSession session, string dataFile = "iam-data-single-query.tql") {
         string insertQuery = File.ReadAllText(dataFile);
-        using (ITypeDBSession session = database.Session(SessionType.Data)) {
-            using (ITypeDBTransaction transaction = session.Transaction(TransactionType.Write)) {
-                Console.WriteLine("Loading data...");
-                IEnumerable<IConceptMap> response = transaction.Query.Insert(insertQuery);
-                int count = response.Count();
-                transaction.Commit();
-                Console.WriteLine("OK");
-            }
+        using (ITypeDBTransaction tx = session.Transaction(TransactionType.Write)) {
+            Console.WriteLine("Loading data...");
+            IEnumerable<IConceptMap> response = tx.Query.Insert(insertQuery);
+            int count = response.Count();
+            tx.Commit();
+            Console.WriteLine("OK");
         }
     }
     // end::db-dataset-setup[]
     // tag::create_new_db[]
-    bool CreateDatabase(ITypeDBDriver driver, string dbName) {
+    static bool CreateDatabase(ITypeDBDriver driver, string dbName) {
         Console.WriteLine("Creating a new database...");
         driver.Databases.Create(dbName);
         Console.WriteLine("OK");
-        IDatabase database = driver.Databases.Get(dbName);
-        DbSchemaSetup(database);
-        DbDatasetSetup(database);
+        using (ITypeDBSession session = driver.Session(dbName, SessionType.Schema)) {
+            DbSchemaSetup(session);
+        }
+        using (ITypeDBSession session = driver.Session(dbName, SessionType.Data)) {
+            DbDatasetSetup(session);
+        }
         return true;
     }
     // end::create_new_db[]
     // tag::replace_db[]
-    bool ReplaceDatabase(ITypeDBDriver driver, string dbName) {
+    static bool ReplaceDatabase(ITypeDBDriver driver, string dbName) {
         Console.WriteLine("Deleting an existing database...");
         driver.Databases.Get(dbName).Delete();
         Console.WriteLine("OK");
@@ -65,39 +79,49 @@ class WelcomeToTypeDB
     }
     // end::replace_db[]
     // tag::test-db[]
-    bool DbCheck(IDatabase database) {
-        using (ITypeDBSession session = database.Session(SessionType.Data)) {
-            using (ITypeDBTransaction transaction = session.Transaction(TransactionType.Read)) {
-                Console.WriteLine("Testing the database...");
-                string testQuery = "match $u isa user; get $u; count;";
-                long result = transaction.Query.Aggregate(testQuery).As<long>();
-                if (result == 3) {
-                    Console.WriteLine("Passed");
-                    return true;
-                } else {
-                    Console.WriteLine($"Failed with the result: {result}\nExpected result: 3.");
-                    return false;
-                }
+    static bool DbCheck(ITypeDBSession session) {
+        using (ITypeDBTransaction tx = session.Transaction(TransactionType.Read)) {
+            Console.WriteLine("Testing the database...");
+            string testQuery = "match $u isa user; get $u; count;";
+            long result = tx.Query.GetAggregate(testQuery).Resolve().AsLong();
+            if (result == 3) {
+                Console.WriteLine("Passed");
+                return true;
+            } else {
+                Console.WriteLine($"Failed with the result: {result}\nExpected result: 3.");
+                return false;
             }
         }
     }
     // end::test-db[]
     // tag::db-setup[]
-    bool DbSetup(ITypeDBDriver driver, string dbName, bool dbReset = false) {
+    static bool DbSetup(ITypeDBDriver driver, string dbName, bool dbReset = false) {
         Console.WriteLine($"Setting up the database: {dbName}");
         if (driver.Databases.Contains(dbName)) {
-            if (dbReset || ConsoleYesNoPrompt("Found a pre-existing database. Do you want to replace it? (Y/N)")) {
-                return ReplaceDatabase(driver, dbName);
+            if (dbReset) {
+                ReplaceDatabase(driver, dbName);
             } else {
-                Console.WriteLine("Reusing an existing database.");
+                Console.Write("Found a pre-existing database. Do you want to replace it? (Y/N) ");
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                Console.WriteLine();
+                if (keyInfo.Key == ConsoleKey.Y)
+                    {
+                        ReplaceDatabase(driver, dbName);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Reusing an existing database.");
+                    }
             }
         } else {
-            return CreateDatabase(driver, dbName);
+            CreateDatabase(driver, dbName);
         }
-        IDatabase database = driver.Databases.Get(dbName);
-        return DbCheck(database);
+        using (ITypeDBSession session = driver.Session(dbName, SessionType.Data)) {
+            return DbCheck(session);
+        }
     }
     // end::db-setup[]
+    /**
     // tag::json[]
     void PrintJSON(JObject json) {
         if (json.IsString) {
@@ -111,17 +135,19 @@ class WelcomeToTypeDB
         }
     }
     // end::json[]
+    **/
     // tag::fetch[]
-    List<JObject> FetchAllUsers(IDatabase database) {
-        List<JObject> users = new List<JObject>();
-        using (ITypeDBSession session = database.Session(SessionType.Data)) {
-            using (ITypeDBTransaction transaction = session.Transaction(TransactionType.Read)) {
-                IEnumerable<JObject> queryResult = transaction.Query.FetchJson("match $u isa user; fetch $u: full-name, email;");
+    static List<JObject> FetchAllUsers(ITypeDBDriver driver, string dbName) {
+        List<JObject> users = new();
+        using (ITypeDBSession session = driver.Session(dbName, SessionType.Data)) {
+            using (ITypeDBTransaction tx = session.Transaction(TransactionType.Read)) {
+                IEnumerable<JObject> queryResult = tx.Query.Fetch("match $u isa user; fetch $u: full-name, email;");
                 int c = 1;
                 foreach (JObject user in queryResult) {
                     users.Add(user);
                     Console.WriteLine($"User #{c++} ");
-                    PrintJSON(user);
+                    Console.WriteLine(JsonConvert.SerializeObject(user, Formatting.Indented));
+                    // PrintJSON(user);
                     Console.WriteLine();
                 }
             }
@@ -130,30 +156,32 @@ class WelcomeToTypeDB
     }
     // end::fetch[]
     // tag::insert[]
-    IEnumerable<IConceptMap> InsertNewUser(IDatabase database, string name, string email) {
+    static IEnumerable<IConceptMap> InsertNewUser(ITypeDBDriver driver, string dbName, string name, string email) {
         List<IConceptMap> response = new List<IConceptMap>();
-        using (ITypeDBSession session = database.Session(SessionType.Data)) {
-            using (ITypeDBTransaction transaction = session.Transaction(TransactionType.Write)) {
-                string query = $"insert $p isa person, has full-name {name}, has email {email};";
-                IEnumerable<IConceptMap> result = transaction.Query.Insert(query);
+        using (ITypeDBSession session = driver.Session(dbName, SessionType.Data)) {
+            using (ITypeDBTransaction tx = session.Transaction(TransactionType.Write)) {
+                string query = $"insert $p isa person, has full-name '{name}', has email '{email}';";
+                IEnumerable<IConceptMap> result = tx.Query.Insert(query);
                 foreach (IConceptMap conceptMap in result) {
-                    string retrievedName = conceptMap.Get("fn").AsAttribute().Value.AsString();
-                    string retrievedEmail = conceptMap.Get("e").AsAttribute().Value.AsString();
+                    string retrievedName = conceptMap.Get("_0").AsAttribute().Value.AsString();
+                    string retrievedEmail = conceptMap.Get("_1").AsAttribute().Value.AsString();
                     Console.WriteLine($"Added new user. Name: {retrievedName}, E-mail: {retrievedEmail}");
                 }
-                transaction.Commit();
+                tx.Commit();
             }
         }
         return response;
     }
     // end::insert[]
     // tag::get[]
-    List<string> GetFilesByUser(IDatabase database, string name, bool inference = false) {
+    static List<string> GetFilesByUser(ITypeDBDriver driver, string dbName, string name, bool inference = false) {
+        TypeDBOptions options = new();
+        options.Infer(true);
         List<string> files = new List<string>();
-        using (ITypeDBSession session = database.Session(SessionType.Data)) {
-            using (ITypeDBTransaction transaction = session.Transaction(TransactionType.Read)) {
+        using (ITypeDBSession session = driver.Session(dbName, SessionType.Data)) {
+            using (ITypeDBTransaction tx = session.Transaction(TransactionType.Read, options)) {
                 string query = $"match $u isa user, has full-name '{name}'; get;";
-                IEnumerable<IConceptMap> users = transaction.Query.Get(query);
+                IEnumerable<IConceptMap> users = tx.Query.Get(query);
                 int userCount = users.Count();
                 if (userCount > 1) {
                     Console.WriteLine("Error: Found more than one user with that name.");
@@ -167,7 +195,7 @@ class WelcomeToTypeDB
                         $pa($o, $va) isa access;
                         $va isa action, has name 'view_file';
                         get $fp; sort $fp asc;";
-                    IEnumerable<IConceptMap> response = transaction.Query.Get(fetchQuery);
+                    IEnumerable<IConceptMap> response = tx.Query.Get(fetchQuery);
                     int resultCounter = 0;
                     foreach (IConceptMap cm in response) {
                         resultCounter++;
@@ -186,21 +214,22 @@ class WelcomeToTypeDB
     }
     // end::get[]
     // tag::update[]
-    int UpdateFilePath(IDatabase database, string oldPath, string newPath) {
+    static int UpdateFilePath(ITypeDBDriver driver, string dbName, string oldPath, string newPath) {
         int count = 0;
-        using (ITypeDBSession session = database.Session(SessionType.Data)) {
-            using (ITypeDBTransaction transaction = session.Transaction(TransactionType.Write)) {
+        using (ITypeDBSession session = driver.Session(dbName, SessionType.Data)) {
+            using (ITypeDBTransaction tx = session.Transaction(TransactionType.Write)) {
                 string updateQuery = $@"
                     match
-                    $f isa file, has path '{oldPath}';
+                    $f isa file, has path $old;
+                    $old == '{oldPath}';
                     delete
-                    $f has path '{oldPath}';
+                    $f has $old;
                     insert
                     $f has path '{newPath}';";
-                IEnumerable<IConceptMap> response = transaction.Query.Update(updateQuery);
+                List<IConceptMap> response = tx.Query.Update(updateQuery).ToList();
                 count = response.Count();
                 if (count > 0) {
-                    transaction.Commit();
+                    tx.Commit();
                     Console.WriteLine($"Total number of paths updated: {count}.");
                 } else {
                     Console.WriteLine("No matched paths: nothing to update.");
@@ -211,15 +240,15 @@ class WelcomeToTypeDB
     }
     // end::update[]
     // tag::delete[]
-    bool DeleteFile(IDatabase database, string path) {
-        using (ITypeDBSession session = database.Session(SessionType.Data)) {
-            using (ITypeDBTransaction transaction = session.Transaction(TransactionType.Write)) {
+    static bool DeleteFile(ITypeDBDriver driver, string dbName, string path) {
+        using (ITypeDBSession session = driver.Session(dbName, SessionType.Data)) {
+            using (ITypeDBTransaction tx = session.Transaction(TransactionType.Write)) {
                 string matchQuery = $"match $f isa file, has path '{path}'; get;";
-                IEnumerable<IConceptMap> response = transaction.Query.Get(matchQuery);
+                IEnumerable<IConceptMap> response = tx.Query.Get(matchQuery);
                 int count = response.Count();
                 if (count == 1) {
-                    transaction.Query.MatchDelete($"match $f isa file, has path '{path}'; delete $f isa file;").Resolve();
-                    transaction.Commit();
+                    tx.Query.Delete($"match $f isa file, has path '{path}'; delete $f isa file;").Resolve();
+                    tx.Commit();
                     Console.WriteLine("The file has been deleted.");
                     return true;
                 } else if (count > 1) {
@@ -236,34 +265,34 @@ class WelcomeToTypeDB
     }
     // end::delete[]
     // tag::queries[]
-    void Queries(ITypeDBDriver driver, string dbName) {
+    static void Queries(ITypeDBDriver driver, string dbName) {
         Console.WriteLine("\nRequest 1 of 6: Fetch all users as JSON objects with full names and emails");
-        List<JObject> users = FetchAllUsers(driver.Databases.Get(dbName));
+        List<JObject> users = FetchAllUsers(driver, dbName);
 
         string newName = "Jack Keeper";
         string newEmail = "jk@typedb.com";
         Console.WriteLine("\nRequest 2 of 6: Add a new user with the full-name " + newName + " and email " + newEmail);
-        InsertNewUser(driver.Databases.Get(dbName), newName, newEmail);
+        InsertNewUser(driver, dbName, newName, newEmail);
 
         string name = "Kevin Morrison";
         Console.WriteLine("\nRequest 3 of 6: Find all files that the user " + name + " has access to view (no inference)");
-        List<string> noFiles = GetFilesByUser(driver.Databases.Get(dbName), name);
+        List<string> noFiles = GetFilesByUser(driver, dbName, name);
 
         Console.WriteLine("\nRequest 4 of 6: Find all files that the user " + name + " has access to view (with inference)");
-        List<string> files = GetFilesByUser(driver.Databases.Get(dbName), name, true);
+        List<string> files = GetFilesByUser(driver, dbName, name, true);
 
         string oldPath = "lzfkn.java";
         string newPath = "lzfkn2.java";
         Console.WriteLine("\nRequest 5 of 6: Update the path of a file from " + oldPath + " to " + newPath);
-        int updatedFiles = UpdateFilePath(driver.Databases.Get(dbName), oldPath, newPath);
+        int updatedFiles = UpdateFilePath(driver, dbName, oldPath, newPath);
 
         string filePath = "lzfkn2.java";
         Console.WriteLine("\nRequest 6 of 6: Delete the file with path " + filePath);
-        bool deleted = DeleteFile(driver.Databases.Get(dbName), filePath);
+        bool deleted = DeleteFile(driver, dbName, filePath);
     }
     // end::queries[]
     // tag::connection[]
-    ITypeDBDriver ConnectToTypeDB(Edition typedbEdition, string addr, string username = CLOUD_USERNAME, string password = CLOUD_PASSWORD, bool encryption = true) {
+    static ITypeDBDriver ConnectToTypeDB(Edition typedbEdition, string addr, string username = CLOUD_USERNAME, string password = CLOUD_PASSWORD, bool encryption = true) {
         switch (typedbEdition) {
             case Edition.Core:
                 return TypeDB.Driver.Drivers.CoreDriver(addr);
@@ -276,18 +305,19 @@ class WelcomeToTypeDB
     // end::connection[]
     // tag::main[]
     static void Main(string[] args) {
-        ITypeDBDriver driver = ConnectToTypeDB(TYPEDB_EDITION, SERVER_ADDR);
-        if (driver.IsOpen()) {
-            if (DbSetup(driver, DB_NAME)) {
-                Queries(driver, DB_NAME);
-                Environment.Exit(0);
+        using (ITypeDBDriver driver = ConnectToTypeDB(TYPEDB_EDITION, SERVER_ADDR)) {
+            if (driver.IsOpen()) {
+                if (DbSetup(driver, DB_NAME)) {
+                    Queries(driver, DB_NAME);
+                    Environment.Exit(0);
+                } else {
+                    Console.Error.WriteLine("Failed to set up the database. Terminating...");
+                    Environment.Exit(1);
+                }
             } else {
-                Console.Error.WriteLine("Failed to set up the database. Terminating...");
+                Console.Error.WriteLine("Failed to connect to TypeDB server. Terminating...");
                 Environment.Exit(1);
             }
-        } else {
-            Console.Error.WriteLine("Failed to connect to TypeDB server. Terminating...");
-            Environment.Exit(1);
         }
     }
     // end::main[]
